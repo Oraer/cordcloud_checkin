0fd1dc4599cf408a7b518718e6bdef1dfbca778a
Oraer <68904070+Oraer@users.noreply.github.com>
2023-08-06 07:50:49 +0000

    update

--- AutoCheckin.py
+++ AutoCheckin.py
@@ -8,6 +8,7 @@ import zipfile
 import requests
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+import time
 
 def unzip(src, dest):
     if os.path.exists(src):
@@ -139,7 +140,11 @@ def start_checkin(username, password, url, chrome_path, chromedriver_path, debug
         password_input = driver.find_element(by=By.NAME, value="Password")
         password_input.send_keys(password)
         driver.find_element(by=By.ID, value="login").click()
+
+        print("sleep 10s, waiting Login")
+        time.sleep(10)
         print("#"*40 + "\nFirst Step Login Login Login\n" + "#"*40)
+
         if debug:
             print(f"get page url: f{driver.current_url}")
             print(f"page sources: f{driver.page_source}")
@@ -166,10 +171,11 @@ def start_checkin(username, password, url, chrome_path, chromedriver_path, debug
             print(f"post url: {response.url}")
             print(f"post status code: {response.status_code}")
             print(f"post headers: {response.headers}")
-            print(f"post text: {response.text}")
-        print(response.json())
+            # print(f"post text: {response.text}")
+        print((f"post response.json: {response.json()}")
 
     except Exception as e:
+        print("Check in Failed.")
         print(e)
     finally:
         driver.quit()
